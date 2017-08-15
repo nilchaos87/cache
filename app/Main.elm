8774e9a2974be66d1a4f7b609a9d59251f69bb41
nilchaos87 <justthebit@inorbit.com>
2017-08-14 18:40:26 -0700

    Began styling (WIP)

--- app/Main.elm
+++ app/Main.elm
@@ -4,7 +4,7 @@ import Html exposing (Html, program, div, input, button, text)
 import Html.Attributes exposing (value)
 import Html.Events exposing (onInput, onClick)
 import Http
-import Css exposing (Style, displayFlex, flexFlow2, noWrap, column, alignItems, stretch)
+import Css exposing (Style, displayFlex, flexFlow2, noWrap, column, alignItems, stretch, flex, position, absolute, top, right, bottom, left, px, int, backgroundColor, hex)
 import Data.Balance
 
 
@@ -101,17 +101,37 @@ viewStyles =
     [ displayFlex
     , flexFlow2 noWrap column
     , alignItems stretch
+    , position absolute
+    , top (px 0)
+    , right (px 0)
+    , bottom (px 0)
+    , left (px 0)
     ]
 
 
 view : Model -> Html Msg
 view model =
-    div [ style viewStyles ]
-        [ div [] (List.map walletView model.wallets)
-        , div [] [ input [ onInput Input, value model.newAddress ] [], button [ onClick Add ] [ text "Add" ] ]
-        ]
+    div [ style viewStyles ] <| List.append (List.map walletView model.wallets) [ newWalletView model.newAddress ]
+
+
+walletViewStyle : List Style
+walletViewStyle =
+    [ flex (int 3)
+    ]
 
 
 walletView : Wallet -> Html Msg
 walletView wallet =
-    div [] [ button [ onClick <| Remove wallet.address ] [ text "-" ], text (wallet.address ++ " - " ++ (toString wallet.balance)) ]
+    div [ style walletViewStyle ] [ button [ onClick <| Remove wallet.address ] [ text "-" ], text (wallet.address ++ " - " ++ (toString wallet.balance)) ]
+
+
+newWalletViewStyle : List Style
+newWalletViewStyle =
+    [ flex (int 1)
+    , backgroundColor <| hex "000"
+    ]
+
+
+newWalletView : String -> Html Msg
+newWalletView newAddress =
+    div [ style newWalletViewStyle ] [ input [ onInput Input, value newAddress ] [], button [ onClick Add ] [ text "Add" ] ]
