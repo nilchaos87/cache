2bb1082141ca8307092cb735010c39c9f9075342
nilchaos87 <justthebit@inorbit.com>
2017-08-26 08:20:57 -0700

    Add deploy script for Travis

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+rm -rf dist || exit 0;
+mkdir dist;
+
+cd dist
+git init
+
+git config user.name "Travis CI"
+git config user.email "justthebit@inorbit.com"
+
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
